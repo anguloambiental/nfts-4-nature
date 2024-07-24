f1d1aac6a7dc418a77618c8ac97e0d5217b7fbe1
Javier Vega <javier.v.m.1994@gmail.com>
2024-07-23 21:03:04 -0600

    map and buttons created, with hover and click function

--- nfts-4-nature/app/Maps/page.tsx
+++ nfts-4-nature/app/Maps/page.tsx
@@ -1,18 +1,235 @@
-import ImageButton from "../Components/ImageButton";
+"use client"
 
 const Maps = () => {
 
     const style = {
         "width": "100%",
         "height": "100%",
-        "object-fit": "contain"
+        "object-fit": "contain",
+        "max-width": "1208px",
+        "max-height": "770px",
     }
+
+    function getNFT(element: any){
+        console.log('click')
+        console.log(element.attributes['data-token'].value)
+    }
+
+    function renderNFT(element: any){
+        console.log(element.attributes['data-token'].value)
+    }
+
     return (
         <div className="main-cont column-cont">
             <h1>MAPS</h1>
             <h3>IZTAPALAPA</h3>
             <span>chose the park to check out available units</span>
-            <img src="images/CerroDeLaEstrella.png" style={style} alt="Cerro de la Estrella" />
+            <div className="park_container">
+                <div className="button_container">
+                    <div className="hex_button"></div>
+                    <div className="hex_button"></div>
+                    <div data-token={Math.floor(Math.random() * 6)}
+                    onClick={e => getNFT(e.target)}
+                    onMouseOver={e => renderNFT(e.target)}
+                    className="hex_button active smaller" />
+                    <div data-token={Math.floor(Math.random() * 6)}
+                    onClick={e => getNFT(e.target)}
+                    onMouseOver={e => renderNFT(e.target)}
+                    className="hex_button active" />
+                    <div data-token={Math.floor(Math.random() * 6)}
+                    onClick={e => getNFT(e.target)}
+                    onMouseOver={e => renderNFT(e.target)}
+                    className="hex_button active" />
+                    <div className="hex_button smaller" />
+                    <div className="hex_button smaller" />
+                    <div className="hex_button" />
+                    <div className="hex_button" />
+                    <div className="hex_button" />
+                    <div className="hex_button smaller" />
+                    <div data-token={Math.floor(Math.random() * 6)}
+                    onClick={e => getNFT(e.target)}
+                    onMouseOver={e => renderNFT(e.target)}
+                    className="hex_button active" />
+                    <div data-token={Math.floor(Math.random() * 6)}
+                    onClick={e => getNFT(e.target)}
+                    onMouseOver={e => renderNFT(e.target)}
+                    className="hex_button active bigger" />
+                    <div data-token={Math.floor(Math.random() * 6)}
+                    onClick={e => getNFT(e.target)}
+                    onMouseOver={e => renderNFT(e.target)}
+                    className="hex_button active" />
+                    <div data-token={Math.floor(Math.random() * 6)}
+                    onClick={e => getNFT(e.target)}
+                    onMouseOver={e => renderNFT(e.target)}
+                    className="hex_button active" />
+                    <div className="hex_button" />
+                    <div className="hex_button" />
+                    <div className="hex_button" />
+                    <div className="hex_button" />
+                    <div data-token={Math.floor(Math.random() * 6)}
+                    onClick={e => getNFT(e.target)}
+                    onMouseOver={e => renderNFT(e.target)}
+                    className="hex_button active smaller" />
+                    <div data-token={Math.floor(Math.random() * 6)}
+                    onClick={e => getNFT(e.target)}
+                    onMouseOver={e => renderNFT(e.target)}
+                    className="hex_button active" />
+                    <div data-token={Math.floor(Math.random() * 6)}
+                    onClick={e => getNFT(e.target)}
+                    onMouseOver={e => renderNFT(e.target)}
+                    className="hex_button active" />
+                    <div data-token={Math.floor(Math.random() * 6)}
+                    onClick={e => getNFT(e.target)}
+                    onMouseOver={e => renderNFT(e.target)}
+                    className="hex_button active" />
+                    <div data-token={Math.floor(Math.random() * 6)}
+                    onClick={e => getNFT(e.target)}
+                    onMouseOver={e => renderNFT(e.target)}
+                    className="hex_button active" />
+                    <div className="hex_button" />
+                    <div className="hex_button" />
+                    <div className="hex_button" />
+                    <div className="hex_button" />
+                    <div className="hex_button smaller" />
+                    <div data-token={Math.floor(Math.random() * 6)}
+                    onClick={e => getNFT(e.target)}
+                    onMouseOver={e => renderNFT(e.target)}
+                    className="hex_button active bigger" />
+                    <div data-token={Math.floor(Math.random() * 6)}
+                    onClick={e => getNFT(e.target)}
+                    onMouseOver={e => renderNFT(e.target)}
+                    className="hex_button active" />
+                    <div data-token={Math.floor(Math.random() * 6)}
+                    onClick={e => getNFT(e.target)}
+                    onMouseOver={e => renderNFT(e.target)}
+                    className="hex_button active" />
+                    <div data-token={Math.floor(Math.random() * 6)}
+                    onClick={e => getNFT(e.target)}
+                    onMouseOver={e => renderNFT(e.target)}
+                    className="hex_button active" />
+                    <div className="hex_button" />
+                    <div className="hex_button" />
+                    <div className="hex_button" />
+                    <div className="hex_button bigger" />
+                    <div className="hex_button" />
+                    <div data-token={Math.floor(Math.random() * 6)}
+                    onClick={e => getNFT(e.target)}
+                    onMouseOver={e => renderNFT(e.target)}
+                    className="hex_button active smaller" />
+                    <div data-token={Math.floor(Math.random() * 6)}
+                    onClick={e => getNFT(e.target)}
+                    onMouseOver={e => renderNFT(e.target)}
+                    className="hex_button active" />
+                    <div data-token={Math.floor(Math.random() * 6)}
+                    onClick={e => getNFT(e.target)}
+                    onMouseOver={e => renderNFT(e.target)}
+                    className="hex_button active" />
+                    <div data-token={Math.floor(Math.random() * 6)}
+                    onClick={e => getNFT(e.target)}
+                    onMouseOver={e => renderNFT(e.target)}
+                    className="hex_button active smaller" />
+                    <div data-token={Math.floor(Math.random() * 6)}
+                    onClick={e => getNFT(e.target)}
+                    onMouseOver={e => renderNFT(e.target)}
+                    className="hex_button active" />
+                    <div className="hex_button" />
+                    <div className="hex_button" />
+                    <div className="hex_button smaller" />
+                    <div className="hex_button smaller" />
+                    <div className="hex_button" />
+                    <div data-token={Math.floor(Math.random() * 6)}
+                    onClick={e => getNFT(e.target)}
+                    onMouseOver={e => renderNFT(e.target)}
+                    className="hex_button active" />
+                    <div data-token={Math.floor(Math.random() * 6)}
+                    onClick={e => getNFT(e.target)}
+                    onMouseOver={e => renderNFT(e.target)}
+                    className="hex_button active bigger" />
+                    <div data-token={Math.floor(Math.random() * 6)}
+                    onClick={e => getNFT(e.target)}
+                    onMouseOver={e => renderNFT(e.target)}
+                    className="hex_button active bigger" />
+                    <div data-token={Math.floor(Math.random() * 6)}
+                    onClick={e => getNFT(e.target)}
+                    onMouseOver={e => renderNFT(e.target)}
+                    className="hex_button active bigger" />
+                    <div className="hex_button smaller" />
+                    <div className="hex_button" />
+                    <div className="hex_button" />
+                    <div className="hex_button smaller" />
+                    <div data-token={Math.floor(Math.random() * 6)}
+                    onClick={e => getNFT(e.target)}
+                    onMouseOver={e => renderNFT(e.target)}
+                    className="hex_button active bigger" />
+                    <div data-token={Math.floor(Math.random() * 6)}
+                    onClick={e => getNFT(e.target)}
+                    onMouseOver={e => renderNFT(e.target)}
+                    className="hex_button active" />
+                    <div data-token={Math.floor(Math.random() * 6)}
+                    onClick={e => getNFT(e.target)}
+                    onMouseOver={e => renderNFT(e.target)}
+                    className="hex_button active" />
+                    <div data-token={Math.floor(Math.random() * 6)}
+                    onClick={e => getNFT(e.target)}
+                    onMouseOver={e => renderNFT(e.target)}
+                    className="hex_button active" />
+                    <div data-token={Math.floor(Math.random() * 6)}
+                    onClick={e => getNFT(e.target)}
+                    onMouseOver={e => renderNFT(e.target)}
+                    className="hex_button active" />
+                    <div data-token={Math.floor(Math.random() * 6)}
+                    onClick={e => getNFT(e.target)}
+                    onMouseOver={e => renderNFT(e.target)}
+                    className="hex_button active" />
+                    <div className="hex_button" />
+                    <div className="hex_button" />
+                    <div data-token={Math.floor(Math.random() * 6)}
+                    onClick={e => getNFT(e.target)}
+                    onMouseOver={e => renderNFT(e.target)}
+                    className="hex_button active" />
+                    <div data-token={Math.floor(Math.random() * 6)}
+                    onClick={e => getNFT(e.target)}
+                    onMouseOver={e => renderNFT(e.target)}
+                    className="hex_button active" />
+                    <div data-token={Math.floor(Math.random() * 6)}
+                    onClick={e => getNFT(e.target)}
+                    onMouseOver={e => renderNFT(e.target)}
+                    className="hex_button active" />
+                    <div className="hex_button smaller" />
+                    <div className="hex_button" />
+                    <div data-token={Math.floor(Math.random() * 6)}
+                    onClick={e => getNFT(e.target)}
+                    onMouseOver={e => renderNFT(e.target)}
+                    className="hex_button active" />
+                    <div data-token={Math.floor(Math.random() * 6)}
+                    onClick={e => getNFT(e.target)}
+                    onMouseOver={e => renderNFT(e.target)}
+                    className="hex_button active" />
+                    <div className="hex_button" />
+                    <div className="hex_button" />
+                    <div data-token={Math.floor(Math.random() * 6)}
+                    onClick={e => getNFT(e.target)}
+                    onMouseOver={e => renderNFT(e.target)}
+                    className="hex_button active half-heigh" />
+                    <div data-token={Math.floor(Math.random() * 6)}
+                    onClick={e => getNFT(e.target)}
+                    onMouseOver={e => renderNFT(e.target)}
+                    className="hex_button active half-heigh" />
+                    <div className="hex_button" />
+                    <div className="hex_button" />
+                    <div className="hex_button" />
+                    <div data-token={Math.floor(Math.random() * 6)}
+                    onClick={e => getNFT(e.target)}
+                    onMouseOver={e => renderNFT(e.target)}
+                    className="hex_button active" />
+                    <div data-token={Math.floor(Math.random() * 6)}
+                    onClick={e => getNFT(e.target)}
+                    onMouseOver={e => renderNFT(e.target)}
+                    className="hex_button active" />
+                    <div className="hex_button" />
+                </div>
+                <img src="images/CerroDeLaEstrella.png" style={style} alt="Cerro de la Estrella" />
+            </div>
         </div>
     )
 }
--- nfts-4-nature/app/page.tsx
+++ nfts-4-nature/app/page.tsx
@@ -1,95 +1,42 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client"
 
-export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+import { useRouter } from 'next/navigation'
+import { LogIn } from './nft_api'
+const LogInPage = () => {
+
+    let message: void | string
+    const router = useRouter()
+    async function signUp(){
+        const accepted = await LogIn()
+        if (accepted != 1) {
+            message = accepted == 0 ? router.push('/NoWallet') : 'Rejected'
+        }else{
+            router.push('/Home')
+        }
+        console.log(message)
+    }
+
+    function noWallet(){
+        router.push('/NoWallet')
+    }
+
+    return (
+        <div className='main-cont'>
+            <div className="log-in-container">
+                <div className="log-in-title row-cont">
+                    <h1 className='title'>CEIBA 3.0</h1>
+                </div>
+                <div className="login-image column-cont">
+                    <img src="/images/Main.png" alt="libelula" />
+                    <h3 className='title'>NFTS FOR NATURE</h3>
+                </div>
+                <div className='space-around-cont buttons-cont'>
+                    <button className='square-button' onClick={signUp}>Connect Wallet</button>
+                    <button className='square-button' onClick={noWallet}>Continue Without Wallet</button>
+                </div>
+            </div>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+    )
 }
+
+export default LogInPage;
